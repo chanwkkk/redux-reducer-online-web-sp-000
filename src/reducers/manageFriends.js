499d8b3a4c282b388a539d789055f848299cac09
Yingqi Chen <chanwk18@hotmail.com>
2020-02-23 00:53:05 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,5 +2,7 @@ export function manageFriends(state, action){
   switch(action.type){
     case "ADD_FRIEND":
       return {...state.friends, action.friend}
+    case "REMOVE_FRIEND"
+      return {state.friends.filter(f=>f.id!=(action.id))}
   }
 }
