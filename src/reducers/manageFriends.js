4deb654b8f27bb02bf3d12e1b85c1f78189348e2
Yingqi Chen <chanwk18@hotmail.com>
2020-02-23 01:04:06 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,8 @@
 export function manageFriends(state, action){
   switch(action.type){
     case "ADD_FRIEND":
-      return Object.assign({}, state.friends,action.friend)
+      let a = state.friends
+      return a.push(action.friends)
     case "REMOVE_FRIEND":
       return {state.friends.filter(f=>f.id!=(action.id))}
     default: return state
